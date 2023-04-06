0e58fbda6b6962cf5e44a18fe0a5253038a6f041
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-06 15:48:40 +0600

    added pages in App.js, wrote Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contacts from "./pages/Contacts";
+
 
 export default function App() {
   return (
@@ -7,10 +11,9 @@ export default function App() {
 
       <Layout>
         <Routes>
-          <Route path="/" element={<h1>Home</h1>} />
-          <Route path="/about" element={<h1>About</h1>} />
-          <Route path="/contacts" element={<h1>Contacts</h1>} />
-          <Route path="/delivery" element={<h1>Delivery</h1>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contacts" element={<Contacts />} />
 
 
         </Routes>

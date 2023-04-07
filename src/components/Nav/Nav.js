558a0b6b1847b7c161658c2c4469982f45faeafa
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-07 14:23:54 +0600

    edit Nav.js and Nav.css (remove input)

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,12 +1,10 @@
 import { NavLink } from "react-router-dom";
 import "./Nav.css";
-import logo from "../../assets/logo.png"
 
 
 export default function Nav() {
   return (
     <nav className="Nav">
-      <img className="logo" src={logo} alt="logo" />
       <ul>
         <li className="NavItem">
           <NavLink to="/">Home</NavLink>
@@ -17,10 +15,6 @@ export default function Nav() {
         <li className="NavItem">
           <NavLink to="/contacts">Contacts</NavLink>
         </li>
-        <li className="NavItem">
-          <NavLink to="/delivery">Delivery</NavLink>
-        </li>
-        <label><input type="search"/></label>
       </ul>
     </nav>
   );

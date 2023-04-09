37aafb3be76d185b6c8ba4d9434dfa174600f124
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-09 20:34:57 +0600

    Create page category and add content

---
+++ src/pages/Category.js
@@ -0,0 +1,28 @@
+import { useMatch } from "react-router-dom";
+import NotFound from "./NotFound";
+
+export default function Category() {
+  const { params } = useMatch("/categories/:slug");
+
+
+
+  const categories = [
+    { id: 1, name: "Action", slug: "action" },
+    { id: 2, name: "Shooter", slug: "shooter" },
+    { id: 3, name: "Arcade", slug: "arcade" },
+    { id: 4, name: "Slasher", slug: "slasher" },
+    { id: 5, name: "Warrior", slug: "warrior" },
+    { id: 6, name: "Adventure", slug: "adventure" },
+  ];
+  const category = categories.find(
+    (category) => category.slug === params.slug);
+
+  if (!category) {
+    return <NotFound />
+  }
+  return (
+    <div className="Category">
+      <h1>{category.name}</h1>
+    </div>
+  );
+}

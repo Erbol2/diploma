1baea986f0e5ee0e3e92dc23df72ccb506e5727a
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-12 15:21:40 +0600

    edit Category.js remove array and added useContext and AppContext

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -1,20 +1,13 @@
 import { useMatch } from "react-router-dom";
 import NotFound from "./NotFound";
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Category() {
   const { params } = useMatch("/categories/:slug");
 
+  const {categories} = useContext(AppContext);
 
-
-  const categories = [
-    { id: 1, name: "Action", slug: "action" },
-    { id: 2, name: "Shooter", slug: "shooter" },
-    { id: 3, name: "Arcade", slug: "arcade" },
-    { id: 4, name: "Slasher", slug: "slasher" },
-    { id: 5, name: "Warrior", slug: "warrior" },
-    { id: 6, name: "Adventure", slug: "adventure" },
-    { id: 7, name: "Strategy", slug: "strategy" },
-  ];
   const category = categories.find(
     (category) => category.slug === params.slug);
 

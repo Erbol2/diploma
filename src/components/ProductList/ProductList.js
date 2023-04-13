8c36e2accee1490e2687db984844d1bb03ce63b9
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-13 20:57:55 +0600

    import productCollection from Firebase and connect them with component ProductList.js and export to component Category.js

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,13 +1,24 @@
 import "./ProductList.css";
 import { useContext } from "react";
 import { AppContext } from "../../App";
- 
-export default function ProductList () {
-const { products } = useContext(AppContext)
+import { NavLink } from "react-router-dom";
+
+export default function ProductList() {
+  const { products } = useContext(AppContext);
+
+  const output = products.map(product => (
+    <div>
+      <img src={product.picture} alt={product.name} />
+      <NavLink to={'/products/' + product.slug}>
+        {product.name}
+      </NavLink>
+      <span>{product.price}</span>
+    </div>
+  ));
 
   return (
     <div className="ProductList">
-
+      {output}
     </div>
   )
 }

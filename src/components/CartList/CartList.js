19133dff04db17557c9d7b291f85604dac7c3cf2
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-17 15:56:59 +0600

    added eventListener for button which remove this product in CartList.js

--- src/components/CartList/CartList.js
+++ src/components/CartList/CartList.js
@@ -8,11 +8,16 @@ export default function CartList() {
   //получить список товаров и корзинку 
   const { products, cart, setCart } = useContext(AppContext);
 
-  function onQuantityChange(product, qty) {
-    setCart({
-      ...cart,
-      [product.id]: qty
-    })
+  // function onQuantityChange(product, qty) {
+  //   setCart({
+  //     ...cart,
+  //     [product.id]: qty
+  //   })
+  // }
+  function onItemRemove(product) {
+    const newCart = { ...cart };
+    delete newCart[product.id];
+    setCart(newCart);
   }
 
   // получить только ID товаров в корзике
@@ -26,14 +31,14 @@ export default function CartList() {
       <div className="CartItem" key={product.id}>
         <img src={product.picture} alt={product.name} />
         <Link to={"/product/" + product.slug}>{product.name}</Link>
-        <input
+        {/* <input
           type="number"
           value={cart[product.name]}
           min={1}
-          onChange={(event) => onQuantityChange(product, +event.target.value)} />
+          onChange={(event) => onQuantityChange(product, +event.target.value)} /> */}
         <span>{cart[product.id] * product.price} $</span>
-        <button>Remove</button>
-      </div>))
+        <button onClick={()=> onItemRemove(product)}>Remove</button>
+      </div >))
   return (
     <div className="CartList">
       <table>{output}</table>

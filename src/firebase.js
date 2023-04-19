397c3b36c175835929075878f9f4f220dd6c9989
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-19 15:20:38 +0600

    added sign in and sign out in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection } from 'firebase/firestore/lite'
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +18,12 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const auth = getAuth(db);
 
 export const categoryCollection = collection(db, 'categories');
 export const productsCollection = collection(db, 'products');
 export const ordersCollection = collection(db, 'orders');
+
+const provider = new GoogleAuthProvider();
+export const signIn = () => signInWithPopup(auth, provider);
+export const signOut = () => signOut();

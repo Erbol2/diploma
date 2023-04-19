3ab34fb20a46ae6628f5e0fabf454a8144d6ac8d
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-19 14:40:28 +0600

    added function which save  custumer's info

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,8 +1,30 @@
+import { addDoc } from "firebase/firestore/lite";
 import "./OrderForm.css";
+import { ordersCollection } from "../../firebase";
+import { useContext } from "react";
+import { AppContext } from "../../App";
 
 export default function OrderForm() {
+  const { cart, setCart } = useContext(AppContext)
+
+  function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target)
+
+    addDoc(ordersCollection, {
+      name: formData.get('name'),
+      phone: formData.get('phone'),
+      email: formData.get('email'),
+      cart: cart,
+    })
+      .then(doc => {
+        setCart({});
+      })
+  }
+
   return (
-    <form>
+    <form className="OrderForm" onSubmit={onFormSubmit}>
       <h2>Create an order</h2>
       <label>
         Name: <input type="text" name="name" required />
@@ -13,6 +35,7 @@ export default function OrderForm() {
       <label>
         Email: <input type="email" name="email" required />
       </label>
+      <button>Submit</button>
     </form>
   );
 }

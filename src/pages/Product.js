193ef70e39122f1f6a61c6f21689bfe8b0d1a83d
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-23 21:13:07 +0600

    added video in page Product.js

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -15,6 +15,9 @@ export default function Product() {
   return (
     <div className="Product">
       <h1>{product.name}</h1>
+      <video width="500" height="300" controls="controls">
+        <source src={product.video} type="video/mp4"/>
+      </video>
       <img style={{width: 450}} src={product.picture} alt={product.name} />
       <span>{product.price}</span>
     </div>

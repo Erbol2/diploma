9fe3f40ce93067d4a6d5ac753942031c8fc734ca
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-24 15:45:13 +0600

    added page Orders.js in Auth.js

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -2,13 +2,14 @@ import { useContext } from "react";
 import { logIn, logOut } from "../../firebase";
 import "./Auth.css";
 import { AppContext } from "../../App";
+import { Link } from "react-router-dom";
 
 export default function Auth() {
-  const {user} =useContext(AppContext);
+  const { user } = useContext(AppContext);
   return (
     <div className="Auth">
       {!user ? <i onClick={logIn} className="fa-solid fa-right-to-bracket fa-2xl signIn"></i> : null}
-      {user ? <span>{user.displayName}</span> : null}
+      {user ? <span><Link to="./orders">{user.displayName}</Link></span> : null}
       {user ? <i onClick={logOut} className="fa-solid fa-right-from-bracket fa-lg signOff"></i> : null}
     </div>
   )
--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -2,10 +2,11 @@ import { useContext } from "react";
 import "./CartLink.css";
 import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
+
+
 export default function CartLink() {
 
   const { cart } = useContext(AppContext)
-
   const total = Object.values(cart).reduce((acc, num) => acc + num, 0)
   return (
     <div className="CartLink">

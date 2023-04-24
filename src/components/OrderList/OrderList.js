4800703251628cc137145872422a14d6d7b1ba90
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-24 15:39:01 +0600

    Created new component OrderList; added content and import into Order.js

---
+++ src/components/OrderList/OrderList.js
@@ -0,0 +1,38 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import "./OrderList.css";
+
+export default function OrderList() {
+  const { orders, products } = useContext(AppContext);
+
+  if (!orders.length || !products.length) {
+    return "No orders found.";
+  }
+
+  const output = orders.map(order => {
+    const cartOutput = Object.keys(order.cart).map(productId => {
+      const product = products.find(product => product.id === productId);
+
+      return (
+        <div>
+          {product.name}: {product.price} $
+        </div>
+      );
+    })
+
+    return (
+      <div className="Order">
+        <div>Name: {order.name}</div>
+        <div>Phone: {order.phone}</div>
+        <div>Address: {order.address}</div>
+        <div>Cart: {cartOutput}</div>
+      </div>
+    );
+  })
+
+  return (
+    <div className="OrderList">
+      {output}
+    </div>
+  );
+}
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -1,6 +1,9 @@
+import OrderList from "../components/OrderList/OrderList";
+
 export default function Orders() {
   return (
     <div className="Orders">
       <h1>Orders</h1>
+      <OrderList/>
     </div>)
 }

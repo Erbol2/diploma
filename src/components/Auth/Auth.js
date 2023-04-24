eefe5dc1040e489728ee70a833a07567e5f4f4d5
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-24 14:59:01 +0600

    edit component Auth; change funtion name from signIn to logIn and edit Auth.css; added cursor

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { signIn, signOff } from "../../firebase";
+import { logIn, logOut } from "../../firebase";
 import "./Auth.css";
 import { AppContext } from "../../App";
 
@@ -7,9 +7,9 @@ export default function Auth() {
   const {user} =useContext(AppContext);
   return (
     <div className="Auth">
-      {!user ? <i onClick={signIn} className="fa-solid fa-right-to-bracket fa-2xl signIn"></i> : null}
+      {!user ? <i onClick={logIn} className="fa-solid fa-right-to-bracket fa-2xl signIn"></i> : null}
       {user ? <span>{user.displayName}</span> : null}
-      {user ? <i onClick={signOff} className="fa-solid fa-right-from-bracket fa-lg signOff"></i> : null}
+      {user ? <i onClick={logOut} className="fa-solid fa-right-from-bracket fa-lg signOff"></i> : null}
     </div>
   )
 }

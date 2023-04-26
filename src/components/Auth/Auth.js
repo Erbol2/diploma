a827fd554f6465f430fb00659f21d0cc7c7345a5
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-26 13:55:00 +0600

    edit Auth.js: added link for page orders

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -9,7 +9,7 @@ export default function Auth() {
   return (
     <div className="Auth">
       {!user ? <i onClick={logIn} className="fa-solid fa-right-to-bracket fa-2xl signIn"></i> : null}
-      {user ? <span><Link to="./orders">{user.displayName}</Link></span> : null}
+      {user ? <span><Link to="/orders">{user.displayName}</Link></span> : null}
       {user ? <i onClick={logOut} className="fa-solid fa-right-from-bracket fa-lg signOff"></i> : null}
     </div>
   )
--- src/components/CartLink/CartLink.js
+++ src/components/CartLink/CartLink.js
@@ -5,13 +5,12 @@ import { NavLink } from "react-router-dom";
 
 
 export default function CartLink() {
-
   const { cart } = useContext(AppContext)
   const total = Object.values(cart).reduce((acc, num) => acc + num, 0)
   return (
     <div className="CartLink">
       <NavLink to="/cart">
-        <i className="fa-solid fa-cart-shopping fa-bounce fa-xl" style={{ color: "#ffffff", }}>({total})</i>
+        <i className="fa-solid fa-cart-shopping fa-xl" style={{ color: "#ffffff", }}>({total})</i>
       </NavLink>
     </div >
   )

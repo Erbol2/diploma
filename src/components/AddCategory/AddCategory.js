3a8dfbfbf761f15319dfe8eb144bb5dad9a9b62e
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-04-28 14:40:52 +0600

    added Category push

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./AddCategory.css";
+import { addDoc } from "firebase/firestore/lite";
+import { categoryCollection } from "../../firebase";
 
 export default function AddCategory() {
   const [category, setCategory] = useState("")
@@ -8,6 +10,20 @@ export default function AddCategory() {
     setCategory(event.target.value)
   }
   function onAddCategory() {
+    const name = category.trim();
+
+    if (name.length < 5) {
+      alert("Please provide a category name with minimum length of 5 characters.")
+
+    }
+    addDoc(categoryCollection, {
+      name: category.trim(),
+      slug: category.trim()
+        .replace(' ', '-')
+        .toLocaleLowerCase(),
+    }).then(() => {
+      setCategory("");
+    });
   }
 
   return (

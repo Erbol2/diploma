d2ecad252d221609bde54f8ca38ae83837a1eab6
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-06 22:15:14 +0600

    added new image; edit component AboutList; Change the describtion and added styles for elements

--- src/components/AboutList/AboutList.js
+++ src/components/AboutList/AboutList.js
@@ -1,13 +1,23 @@
 import "./AboutList.css";
+import pikachu from "../../assets/pikachu.png"
 
 export default function AboutList() {
   return (
     <div className="AboutList">
-      <h1>Game Store</h1>
-      <h3>
-        Game Store is best choice for you!<br />
-        Here you can find out any game what you want.
-      </h3>
+      <div className="aboutContent">
+        <h1>Game Store</h1>
+        <h3>
+          Welcome to our premier online PC game store, the ultimate destination for all your gaming needs! Step into a world of limitless possibilities and immerse yourself in an unrivaled collection of digital adventures. Our store is designed to cater to gamers of all ages and preferences, offering a diverse selection of games that will satisfy every craving for excitement and entertainment.
+
+          At our online PC game store, you'll find an extensive library featuring both classic and contemporary titles, spanning various genres such as action, adventure, role-playing, strategy, simulation, and more. Whether you're a fan of heart-pounding first-person shooters, captivating open-world epics, mind-bending puzzles, or heartwarming indie gems, we have something special in store for you.
+
+          Browse through our intuitive and user-friendly interface to discover an ever-expanding universe of games. Explore featured releases, popular titles, and hidden gems that await your discovery. Our store ensures seamless navigation, allowing you to effortlessly find the games you desire. With detailed descriptions, screenshots, and user reviews, you can make informed decisions and select the perfect game to suit your preferences.
+        </h3>
+      </div>
+      <div className="aboutImg">
+        <img src={pikachu} alt="pikachu" />
+      </div>
+
     </div>
   )
 }

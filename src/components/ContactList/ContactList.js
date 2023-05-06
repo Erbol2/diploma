40cf932caa651bfee87636aa4c3cb8ad88a57560
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-06 23:13:59 +0600

    edit component ContactList; added photo, icons and added sizes for elements

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,18 +1,42 @@
 import "./ContactList.css";
-
+import pikachu from "../../assets/pikachu.png";
 export default function ContactList() {
   return (
     <div className="ContactList">
-      <h1>Contact</h1>
-      <p>Contact Information:
-
-        Thank you for reaching out to us! We value your interest and would be delighted to assist you. Below, you will find our contact information:
-
-        General Inquiries:
-        For any general questions or inquiries, please feel free to reach out to our dedicated team. We are here to provide you with the information you need.
-
-        Email: info@example.com
-        Phone: +1-XXX-XXXXXXX</p>
+      <div className="content">
+        <p>
+          Thank you for reaching out to us! We value your interest and would be delighted to assist you. Below, you will find our contact information:
+          <br />
+          <br />
+          General Inquiries:
+          For any general questions or inquiries, please feel free to reach out to our dedicated team. We are here to provide you with the information you need.
+          <br />
+          <br />
+          Email: alymbekov2004@gmail.com
+          <br />
+          Phone: +996 (707)-707-707
+          <br />
+          <br />
+          Social Media:
+          Stay connected with us through our social media channels to receive updates, news, and engage with our community.
+          <br />
+          <br />
+          <a href="/">
+            <i className="fa-brands fa-instagram fa-2xl"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-github fa-2xl"></i>
+          </a>
+          <a href="/">
+            <i class="fa-brands fa-linkedin fa-2xl"></i>
+          </a>
+          <br />
+          <br />
+          We appreciate your interest and will respond to your inquiries as promptly as possible. Thank you for choosing us as your trusted source.</p>
+      </div>
+      <div className="img-block">
+        <img src={pikachu} alt="pikachuphone" />
+      </div>
     </div>
   )
 }
--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -3,7 +3,7 @@ import ContactList from "../components/ContactList/ContactList";
 export default function Contacts() {
   return (
     <div className="Contacts">
-      <h1>Contacts</h1>
+      <h1>Contact Information:</h1>
       <ContactList/>
     </div>)
 }

4c44e4e3a2a365f3e083b8ecdc4ef40430387de5
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-09 21:00:36 +0600

    started add adaptation for max-width: 689px; change display: grid; and added new font-size in CartList

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -9,22 +9,24 @@ export default function CategoryList() {
   const { categories } = useContext(AppContext)
 
   const output = categories.map((category) => (
-      <li key={category.id}>
-        <NavLink to={"/categories/" + category.slug}>{category.name}</NavLink>
-        <DeleteCategory category={category} />
-      </li>
-    ));
+    <li key={category.id}>
+      <NavLink to={"/categories/" + category.slug}>{category.name}</NavLink>
+      <DeleteCategory category={category} />
+    </li>
+  ));
 
 
   return (
     <div className="CategoryList">
-      <i className="fa-solid fa-bars fa-beat fa-xl"></i>
-      <h2>Categories</h2>
-      <nav>
-        <h3>Categories</h3>
-        <ul>{output}</ul>
-        <AddCategory />
-      </nav>
+      <div className="aside_nav">
+        <i className="fa-solid fa-bars fa-beat fa-xl"></i>
+        <h2>Categories</h2>
+        <nav>
+          <h3>Categories</h3>
+          <ul>{output}</ul>
+          <AddCategory />
+        </nav>
+      </div>
     </div>
   );
 

71cbe7f6014146d42ee8c99fde40d14081c99b8a
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-10 14:28:01 +0600

    created functions onChange for inputs name, price and picture

--- src/components/AddProduct/AddProduct.js
+++ src/components/AddProduct/AddProduct.js
@@ -1,8 +1,12 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../App";
 
 export default function AddProduct() {
   const { user } = useContext(AppContext);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [picture, setPicture] = useState(null);
+
 
   if (!user || !user.isAdmin) {
     return null;
@@ -14,19 +18,45 @@ export default function AddProduct() {
     event.preventDefault();
     console.log(data.get('picture'));
   }
+  function onChangeName(event) {
+    setName(event.target.value);
+  }
+  function onChangePrice(event) {
+    setPrice(event.target.value);
+  }
+  function onChangePicture(event) {
+    const file = event.target.files[0];
+    setPicture(file);
+  }
 
   return (
     <div className="AddProduct">
       <form onSubmit={onFormSubmit}>
         <h3>Create a new product</h3>
         <label>
-          Name: <input type="text" name="name" required />
+          Name: <input
+            type="text"
+            value={name}
+            name="name"
+            required
+            onChange={onChangeName} />
         </label>
         <label>
-          Price: <input type="number" name="price" step="any" min={0} required /> 
+          Price: <input
+            type="number"
+            name="price"
+            step="any"
+            value={price}
+            min={0}
+            required
+            onChange={onChangePrice} />
         </label>
         <label>
-          Picture: <input type="file" name="picture" required />
+          Picture: <input
+            type="file"
+            name="picture"
+            required
+            onChange={onChangePicture} />
         </label>
         <button>Submit</button>
       </form>

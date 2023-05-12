1474b1fec4a19378455486d1fa2fb03f2820282e
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-12 15:10:13 +0600

    edit component Nav; removed burger_menu and removed media screen in css

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -16,22 +16,6 @@ export default function Nav() {
           <NavLink to="/contacts">Contacts</NavLink>
         </li>
       </ul>
-      <div>
-        <button className="burger_menu">
-          <i className="fa-solid fa-bars fa-beat fa-xl"></i>
-        </button>
-        <ul className="toogle_nav">
-          <li className="NavItem">
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li className="NavItem">
-            <NavLink to="/about">About</NavLink>
-          </li>
-          <li className="NavItem">
-            <NavLink to="/contacts">Contacts</NavLink>
-          </li>
-        </ul>
-      </div>
     </nav >
   );
 }

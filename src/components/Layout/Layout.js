7c762618a7977a3b80f8ad877196b08f98d35df5
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-12 15:16:37 +0600

    import components Drawer and ToggleNav; added function toggleDrawer and edit css

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,13 +5,23 @@ import CategoryList from "../CategoryList/CategoryList";
 import CartLink from "../CartLink/CartLink";
 import Auth from "../Auth/Auth";
 import Footer from "../Footer/Footer";
+import ToggleNav from "../ToggleNav/ToggleNav";
+import Drawer from "../Drawer/Drawer";
+import { useState } from "react";
 
 export default function Layout(props) {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  function toggleDrawer() {
+    setDrawerOpen(!drawerOpen);
+  }
   return (
     <div className="Layout">
       <header>
         <Logo />
         <Nav />
+        <ToggleNav callback={toggleDrawer} />
+        <Drawer open={drawerOpen} toggle={toggleDrawer} />
         <CartLink />
         <Auth />
       </header>

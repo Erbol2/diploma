47e7e0586b52fecd60f8410ebc890358381bff1b
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-13 21:02:31 +0600

    made it so that one product could be shown in several categories

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,25 +1,23 @@
 import "./ProductList.css";
-import { useContext } from "react";
-import { AppContext } from "../../App";
 import { NavLink } from "react-router-dom";
 import AddToCart from "../AddToCart/AddToCart";
 import AddProduct from "../AddProduct/AddProduct";
 import DeleteProduct from "../DeleteProduct/DeleteProduct";
-
+import { useContext } from "react";
+import { AppContext } from "../../App";
 export default function ProductList({ category }) {
   const { products } = useContext(AppContext);
 
-
   const output = products
-    .filter(product => product.category === category.id)
-    .map(product => (
+    .filter((product) =>
+      product.category && product.category.includes(category.id)
+    )
+    .map((product) => (
       <div key={product.id} className="Product">
-        <NavLink to={'/products/' + product.slug}>
+        <NavLink to={"/products/" + product.slug}>
           <img src={product.picture} alt={product.name} />
         </NavLink>
-        <NavLink to={'/products/' + product.slug}>
-          {product.name}
-        </NavLink>
+        <NavLink to={"/products/" + product.slug}>{product.name}</NavLink>
         <span>{product.price} $</span>
         <AddToCart product={product} />
         <DeleteProduct product={product} />
@@ -31,5 +29,5 @@ export default function ProductList({ category }) {
       {output}
       <AddProduct category={category} />
     </div>
-  )
+  );
 }

b96d96dbd4cbde436177586ca4653e346e68dc15
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-15 14:32:42 +0600

    edit component AddCategory; added function isSubmitting

--- src/components/AddCategory/AddCategory.js
+++ src/components/AddCategory/AddCategory.js
@@ -1,36 +1,41 @@
-import { useState } from "react";
-import "./AddCategory.css";
 import { addDoc } from "firebase/firestore";
+import { useContext, useState } from "react";
 import { categoryCollection } from "../../firebase";
-import { useContext } from "react";
 import { AppContext } from "../../App";
 
-export default function AddCategory() {
-  const { user } = useContext(AppContext)
-  const [category, setCategory] = useState("")
+const AddCategory = () => {
+  const { user } = useContext(AppContext);
+  const [category, setCategory] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   if (!user || !user.isAdmin) {
     return null;
   }
 
   function onChangeCategory(event) {
-    setCategory(event.target.value)
+    setCategory(event.target.value);
   }
+
   function onAddCategory() {
     const name = category.trim();
 
     if (name.length < 5) {
-      alert("Please provide a category name with minimum length of 5 characters.");
-      return;
+      alert(
+        "Please provide a category name with minimum length of 5 characters."
+      );
 
+      return;
     }
+
+    setIsSubmitting(true);
+
     addDoc(categoryCollection, {
-      name: category.trim(),
-      slug: category.trim()
-        .replaceAll(' ', '-')
-        .toLocaleLowerCase(),
+      name: name,
+      slug: name.replaceAll(" ", "-").toLocaleLowerCase(),
     }).then(() => {
       setCategory("");
+    }).finally(() => {
+      setIsSubmitting(false);
     });
   }
 
@@ -39,10 +44,15 @@ export default function AddCategory() {
       <input
         size="15"
         type="text"
-        placeholder="Category name"
         value={category}
+        placeholder="Category name"
         onChange={onChangeCategory}
       />
-      <button onClick={onAddCategory}>+</button>
-    </div>)
-}
+      <button onClick={onAddCategory} disabled={isSubmitting}>
+        +
+      </button>
+    </div>
+  );
+};
+
+export default AddCategory;

e96c42fe95fb04d3bed7f25db2aa369f2773a695
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-16 22:13:13 +0600

    changed the error photo and added width for img

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -4,7 +4,7 @@ import error from "../assets/error.png"
 export default function NotFound() {
   return (
     <div className="NotFound">
-        <img src={error} alt="Not Found" />
+      <img src={error} alt="Not Found" width="60%"/>
     </div>
   );
 }

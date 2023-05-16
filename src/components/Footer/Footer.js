7d3e1d07549a144d90b2e01fc1f976ddd9799007
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-16 22:36:22 +0600

    edit component Footer; added content in Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,17 +1,41 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 export default function Footer() {
   return (
     <div className="Footer">
-      <a href="/">
-        <i className="fa-brands fa-instagram"></i>
-      </a>
-      <a href="https://youtu.be/dQw4w9WgXcQ">
-        <i className="fa-brands fa-youtube"></i>
-      </a>
-      <a href="/">
-        <i className="fa-brands fa-github"></i>
-      </a>
+      <div className="top_footer">
+        <div className="txtAbout">
+          <h3>About</h3>
+          <p>"Game Shop: Your one-stop destination for all things gaming. We offer a wide selection of video games, consoles, and accessories. Whether you're a casual gamer or a hardcore enthusiast, we've got you covered. Step into a world of endless fun and excitement at Game Shop!"
+          </p>
+        </div>
+        <div className="QuickLinks">
+          <h3>Quick Links</h3>
+          <ul>
+            <li><Link>About</Link></li>
+            <li><Link>Contacts</Link></li>
+          </ul>
+        </div>
+        <div className="GoUp">
+          <i class="fa-solid fa-arrow-up"></i>
+          <h3>GO UP</h3>
+        </div>
+      </div>
+
+      <div className="bottom_footer">
+        <p>Copyright © 2023, All Right Reserved Erbol</p>
+        <div className="icons">
+          <a href="/">
+            <i className="fa-brands fa-instagram"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-youtube"></i>
+          </a>
+          <a href="/">
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </div></div>
     </div>
   )
 }

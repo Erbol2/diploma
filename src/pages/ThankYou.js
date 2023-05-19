cbca17ae488c32bd87798543a7320460771836bc
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-19 09:58:29 +0600

    edit page ThankYou; added function that scroll to top itself when this page is opened

--- src/pages/ThankYou.js
+++ src/pages/ThankYou.js
@@ -2,8 +2,13 @@ export default function ThankYou() {
   setTimeout(function () {
     window.location.href = "https://erbol-alymbekov.netlify.app";
   }, 5000)
+  window.onload = function() {
+    window.scrollTo(0, 0);
+  }
   return (
-    <div className="ThankYou">
+    <div className="ThankYou" style={{
+      textAlign: "center",
+    }}>
       <h1>Thank you!</h1>
       <p>Your order has been received. Our specialists will contact with you shortly to confirm it.</p>
     </div>

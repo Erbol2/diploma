268156eff4955f963131335884277d16c43e736b
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-19 11:15:11 +0600

    fixed the problem in component Randomizer

--- src/components/Randomizer/Randomizer.js
+++ src/components/Randomizer/Randomizer.js
@@ -6,18 +6,22 @@ import "./Randomizer.css";
 export default function Randomizer() {
   const { products } = useContext(AppContext);
   const [selectedProduct, setSelectedProduct] = useState(null);
+  const [showH2, setShowH2] = useState(true);
 
   const handleProductSelected = () => {
     const randomProduct =
       products[Math.floor(Math.random() * products.length)];
     setSelectedProduct(randomProduct);
+    setShowH2(false);
   };
 
   return (
-    <div className="Randomizer" onClick={handleProductSelected} >
+    <div className="Randomizer" onClick={handleProductSelected}>
       <ProductDisplay product={selectedProduct} />
-      <i className="fa-solid fa-dice fa-2xl" ></i>
-      <h2>Сlick on dice to randomly select a game</h2>
+      <i className="fa-solid fa-dice fa-2xl"></i>
+      {showH2 && (
+        <h2>Click on dice to randomly select a game</h2>
+      )}
     </div>
   );
 }

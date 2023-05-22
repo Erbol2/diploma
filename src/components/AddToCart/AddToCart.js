4d2a78a96ec77658af19447f7eff585acf63a3e6
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-22 14:37:52 +0600

    ecit component AddToCart; added function so that when you click on the button it changes the text and so that it does not change when you reload

--- src/components/AddToCart/AddToCart.js
+++ src/components/AddToCart/AddToCart.js
@@ -1,24 +1,48 @@
-import { useContext } from "react";
+import { useState, useContext, useEffect } from "react";
 import "./AddToCart.css";
 import { AppContext } from "../../App";
 
 export default function AddToCart({ product }) {
   const { cart, setCart } = useContext(AppContext);
+  const [buttonText, setButtonText] = useState("Add to Cart");
+  const [isIconAdded, setIsIconAdded] = useState(false);
+
+  useEffect(() => {
+    const storedCart = localStorage.getItem("cart");
+    if (storedCart) {
+      setCart(JSON.parse(storedCart));
+    }
+  }, [setCart]);
+
+  useEffect(() => {
+    if (cart[product.id]) {
+      setButtonText("Added");
+      setIsIconAdded(true);
+    }
+  }, [cart, product.id]);
 
   function onAddToCartClick() {
-    // const qty = cart[product.id] ? cart[product.id] + 1 : 1;  купить товар много раз 
-    setCart({
-      ...cart, // положить текущее содержимое корзинки
-      [product.id]: 1, // добавить текущий товар
-    });
+    if (!cart[product.id]) {
+      setCart({
+        ...cart,
+        [product.id]: 1,
+      });
+    }
   }
+
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
 
   return (
     <div className="AddToCart">
-      <button onClick={onAddToCartClick} className="addBtn">Add to Cart
-        <i className="fa-solid fa-cart-shopping fa-xl" style={{ color: "#084d54", }}></i>
+      <button onClick={onAddToCartClick} className="addBtn">
+        {buttonText}
+        <i className="fa-solid fa-cart-shopping fa-xl" style={{ color: "#084d54" }}></i>
+        {isIconAdded && (
+          <i className="fa-solid fa-check" style={{ margin: "0 3px -3px 0" }}></i>
+        )}
       </button>
-    
-
-    </div>)
+    </div>
+  );
 }

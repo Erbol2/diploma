63c3512b0baac518e112e4d66b1e859d86792c2a
Erbol Alymbekov <alymbekov2004@gmail.com>
2023-05-24 20:16:05 +0600

    fixed the problem with ProductItem and increased burger menu

--- src/components/ToggleNav/ToggleNav.js
+++ src/components/ToggleNav/ToggleNav.js
@@ -3,7 +3,7 @@ import "./ToggleNav.css";
 function ToggleNav({ callback }) {
   return (
     <button onClick={callback} className="ToggleNav">
-      <i className="fa-solid fa-bars fa-beat fa-xl"></i>
+      <i className="fa-solid fa-bars  fa-2xl"></i>
       <div className="line"></div>
       <div className="line"></div>
       <div className="line"></div>
